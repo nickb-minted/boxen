4d6c392bd4522dc206ca4381634b5d3e959e43c6
Ben P <ben.potts@minted.com>
2014-08-28 13:07:38 -0700

    refactor application install/dockicon stuff

--- modules/minted/manifests/environment.pp
+++ modules/minted/manifests/environment.pp
@@ -1,161 +1,87 @@
 class minted::environment {
 
-  define add_application_to_dock() {
-    include dockutil
-    # We touch a file in /var/db/ when adding a dock icon, and use a custom
-    # fact to check for the presence of these. This way, we avoid re-adding
-    # deleted dock icons on subsequent puppet runs.
-    $dockicons_created_ary = split( $dockicons_created, ',' )
-    unless member( $dockicons_created_ary, $name ) {
-      dockutil::item { "Add ${name}":
-        item => "/Applications/${name}.app/",
-        label => "${name}",
-        action => "add"
-      }
-      file { "/var/db/.puppet_dockicon_created_${name}":
-        ensure => 'present'
+  define install_application($dockicon = false) {
+    include $name
+    if ($dockicon) {
+      include dockutil
+      # We touch a file in /var/db/ when adding a dock icon, and use a custom
+      # fact to check for the presence of these. This way, we avoid re-adding
+      # deleted dock icons on subsequent puppet runs.
+      $dockicons_created_ary = split( $dockicons_created, ',' )
+      unless member( $dockicons_created_ary, $dockicon ) {
+        dockutil::item { "Add ${dockicon}":
+          item => "/Applications/${dockicon}.app/",
+          label => "${dockicon}",
+          action => "add",
+          require => Class[$name],
+        } ->
+        file { "/var/db/.puppet_dockicon_created_${dockicon}":
+          ensure => 'present',
+        }
       }
     }
   }
 
   # Applications and dock icons
 
-  include alfred
-  add_application_to_dock{'Alfred 2':}
-
-  include atom
-  add_application_to_dock{'Atom':}
-
-  include charles
-
-  include chicken_of_the_vnc
-  add_application_to_dock{'Chicken':}
-
-  include chrome
-  add_application_to_dock{'Google Chrome':}
-
-  include clipmenu
-  add_application_to_dock{'ClipMenu':}
-
-  include crashplan
-
-  include cyberduck
-  add_application_to_dock{'CyberDuck':}
-
-  include dash
-  add_application_to_dock{'Dash':}
-
-  include divvy
-  add_application_to_dock{'Divvy':}
-
-  include dockutil
-
-  include dropbox
-
-  include dterm
-
-  include emacs
-  add_application_to_dock { 'Emacs':
-    notify => Exec['brew linkapps']
-  }
-
-  include firefox
-  add_application_to_dock{'Firefox':}
-
-  include flux
-
-  include forklift
-  add_application_to_dock{'Forklift':}
-
-  include github_for_mac
-  add_application_to_dock{'GitHub':}
-
-  include gitx
-  add_application_to_dock{'GitX':}
-
-  include hipchat
-  add_application_to_dock{'HipChat':}
-
-  include induction
-  add_application_to_dock{'Induction':}
-
-  Class['iterm2::stable'] -> Class['iterm2::colors::solarized_dark']
-  Class['iterm2::stable'] -> Class['iterm2::colors::solarized_light']
-  include iterm2::stable
-  include iterm2::colors::solarized_light
-  include iterm2::colors::solarized_dark
-  add_application_to_dock{'iTerm':}
-
-  include java
-
-  include jmeter
-
-  include kindle
-
-  include macvim
-  add_application_to_dock { 'MacVim':
-    notify => Exec['brew linkapps']
-  }
-
-  include nmap
-
-  include onyx
-
-  include open_refine
-
-  include python
-
-  include quicksilver
-  add_application_to_dock{'QuickSilver':}
-
-  include reggy
-  add_application_to_dock{'Reggy':}
-
-  include sequel_pro
-  add_application_to_dock{'Sequel Pro':}
-
-  include silverlight
-
-  include skitch
-  add_application_to_dock{'Skitch':}
-
-  include skype
-  add_application_to_dock{'Skype':}
-
-  include slate
-  add_application_to_dock{'Slate':}
-
-  include sourcetree
-  add_application_to_dock{'SourceTree':}
-
-  include spotify
-  add_application_to_dock{'Spotify':}
-
-  include sublime_text
-  add_application_to_dock{'Sublime Text':}
-
-  include tmux
-
-  include tower
-  add_application_to_dock{'Tower':}
-
-  include tunnelblick
-  add_application_to_dock{'Tunnelblick':}
-
-  include vagrant
-
-  include virtualbox
-  add_application_to_dock{'VirtualBox':}
-
-  include vlc
-
-  include wget
+  install_application{'alfred': dockicon => 'Alfred 2'}
+  install_application{'atom': dockicon => 'Atom'}
+  install_application{'charles':}
+  install_application{'chicken_of_the_vnc': dockicon => 'Chicken'}
+  install_application{'chrome': dockicon => 'Google Chrome'}
+  install_application{'clipmenu': dockicon => 'ClipMenu'}
+  install_application{'crashplan':}
+  install_application{'cyberduck': dockicon => 'CyberDuck'}
+  install_application{'dash': dockicon => 'Dash'}
+  install_application{'divvy': dockicon => 'Divvy'}
+  install_application{'dockutil':}
+  install_application{'dropbox':}
+  install_application{'dterm':}
+  install_application{'emacs': dockicon => 'Emacs', notify => Exec['brew linkapps'] }
+  install_application{'firefox': dockicon => 'Firefox'}
+  install_application{'flux':}
+  install_application{'forklift': dockicon => 'Forklift'}
+  install_application{'github_for_mac': dockicon => 'GitHub'}
+  install_application{'gitx': dockicon => 'GitX'}
+  install_application{'hipchat': dockicon => 'HipChat'}
+  install_application{'induction': dockicon => 'Induction'}
+  install_application{'iterm2::stable': dockicon => 'iTerm'}
+  install_application{'java':}
+  install_application{'jmeter':}
+  install_application{'kindle':}
+  install_application{'macvim': dockicon =>'MacVim', notify => Exec['brew linkapps'] }
+  install_application{'nmap':}
+  install_application{'onyx':}
+  install_application{'open_refine':}
+  install_application{'python':}
+  install_application{'quicksilver': dockicon => 'QuickSilver'}
+  install_application{'reggy': dockicon => 'Reggy'}
+  install_application{'sequel_pro': dockicon => 'Sequel Pro'}
+  install_application{'silverlight':}
+  install_application{'skitch': dockicon => 'Skitch'}
+  install_application{'skype': dockicon => 'Skype'}
+  install_application{'slate': dockicon => 'Slate'}
+  install_application{'sourcetree': dockicon => 'SourceTree'}
+  install_application{'spotify': dockicon => 'Spotify'}
+  install_application{'sublime_text': dockicon => 'Sublime Text'}
+  install_application{'tmux':}
+  install_application{'tower': dockicon => 'Tower'}
+  install_application{'tunnelblick': dockicon => 'Tunnelblick'}
+  install_application{'vagrant':}
+  install_application{'virtualbox': dockicon => 'VirtualBox'}
+  install_application{'vlc':}
+  install_application{'wget':}
 
   dockutil::item { "Add Activity Monitor":
     item => '/Applications/Utilities/Activity Monitor.app/',
     label => "Activity Monitor",
     action => "add"
   }
+
+  Class['iterm2::stable'] -> Class['iterm2::colors::solarized_dark']
+  Class['iterm2::stable'] -> Class['iterm2::colors::solarized_light']
+  include iterm2::colors::solarized_light
+  include iterm2::colors::solarized_dark
 
   # Symlink homebrew installed cocoa apps into /Applications
   exec { 'brew linkapps':
