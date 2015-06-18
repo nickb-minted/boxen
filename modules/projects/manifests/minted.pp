955155b25cade1aa0bea7f334b09568b3c39ce9c
Venkata Cherukuri <venkata.cherukuri@minted.com>
2015-06-17 17:34:59 -0700

    [http://jira.minted.com/browse/INF-840] Automate Setting maxfiles in Yosemite
    [problem]
    1. The existing steps for  setting the max files which did not work
    on Yosemite and did not persist across reboots
    2. The step was manual
    
    [solution]
    1. Use a launchd proporty list (".plist") file and put it in
    /Library/LaunchDaemons/ so that the maxfiles limit is set
    as part of the initialization.
    
    2. Use Puppet templates to define the ".plist" file and update
    the minted puppet module to copy the file to the /Library/LaunchDaemons/
    folder when boxen is run
    
    [test]
    1. Ran the Boxen set up
    2. Ran "launchctl limit" to verify the new changes
    
    [QA]
    N/A

--- modules/projects/manifests/minted.pp
+++ modules/projects/manifests/minted.pp
@@ -2,4 +2,15 @@ class projects::minted {
   boxen::project { 'minted':
     source => 'minted/minted'
   }
+
+  service { "limit.maxfiles":
+    enable => true
+  }
+
+  file { '/Library/LaunchDaemons/limit.maxfiles.plist':
+    content => template('projects/shared/limit.maxfiles.plist.erb'),
+    notify  => Service['limit.maxfiles'],
+    group   => 'wheel',
+    owner   => 'root'
+  }
 }
